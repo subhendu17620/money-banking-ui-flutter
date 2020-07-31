2bca52d51894d4b9c213c1fbd1878ee5e5292b7d
subhendu17620 <subhendu17620@gmail.com>
2020-07-31 13:43:38 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:money_banking_ui/screens/HomeScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
